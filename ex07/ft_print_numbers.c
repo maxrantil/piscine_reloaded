b32d474a1f3b7d2c3805558501941bbf06e1938b
Mqx Rantil <mrantil@c1r5p14.hive.fi>
2021-10-28 17:41:07 +0300

    header to file

--- ex07/ft_print_numbers.c
+++ ex07/ft_print_numbers.c
@@ -1,13 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/28 17:38:03 by mrantil           #+#    #+#             */
+/*   Updated: 2021/10/28 17:40:29 by mrantil          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 int	ft_strlen(char *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
 		i++;
-	return i;
+	return (i);
 }
 
 void	ft_putstr(char *str)
@@ -18,10 +30,4 @@ void	ft_putstr(char *str)
 void	ft_put_numbers(void)
 {
 	ft_putstr("0123456789");
-}
-
-int	main(void)
-{
-	ft_put_numbers();
-	return 0;
 }
