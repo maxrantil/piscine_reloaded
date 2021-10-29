7a48998c5dd4e9ed8fe38038f7c6655a82ed782d
mrantil <mrantil@student.hive.fi>
2021-10-29 15:53:01 +0300

    ft_sort_params

---
+++ ex19/ft_sort_params.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/29 13:08:44 by mrantil           #+#    #+#             */
+/*   Updated: 2021/10/29 15:52:10 by mrantil          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	while ((*s1 || *s2) && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char	*temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	while (argv[i + 1])
+	{
+		if (ft_strcmp(argv[i], argv[i + 1]) > 0)
+		{
+			ft_swap(&argv[i], &argv[i + 1]);
+			i = 0;
+		}
+		i++;
+	}
+	i = 1;
+	while (argv[i])
+	{
+		ft_putstr(argv[i]);
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
