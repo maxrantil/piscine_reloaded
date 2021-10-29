7f4b3d088de9b79b54ff63912e25544a43238eba
mrantil <mrantil@student.hive.fi>
2021-10-29 13:07:48 +0300

    ft_print_params

---
+++ ex18/ft_print_params.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/29 12:31:35 by mrantil           #+#    #+#             */
+/*   Updated: 2021/10/29 13:07:24 by mrantil          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(&argv[i][0]);
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
