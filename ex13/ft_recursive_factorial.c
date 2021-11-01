17ea6493e4da3c7e7a20a875f51e17728ca07acf
mrantil <mrantil@student.hive.fi>
2021-11-01 18:55:13 +0200

    ft_recursive_factorial

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -6,7 +6,7 @@
 /*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 19:11:49 by mrantil           #+#    #+#             */
-/*   Updated: 2021/10/28 19:29:42 by mrantil          ###   ########.fr       */
+/*   Updated: 2021/11/01 18:54:58 by mrantil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ int	ft_recursive_factorial(int nb)
 	int	res;
 
 	res = 1;
-	if (nb == 1)
+	if (nb == 1 || nb == 0)
 		return (1);
 	else if (nb > 1 && nb < 13)
 	{
