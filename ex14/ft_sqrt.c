3bd5ae2c2da411cb5d90c9fd38941cecdc2e1030
mrantil <mrantil@student.hive.fi>
2021-11-01 19:04:06 +0200

    ft_sqrt

--- ex14/ft_sqrt.c
+++ ex14/ft_sqrt.c
@@ -6,7 +6,7 @@
 /*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 09:55:47 by mrantil           #+#    #+#             */
-/*   Updated: 2021/10/29 10:43:21 by mrantil          ###   ########.fr       */
+/*   Updated: 2021/11/01 19:03:45 by mrantil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@ int	ft_sqrt(int nb)
 	int	i;
 
 	i = 0;
-	if (nb == 0)
-		return (1);
 	if (nb < 0)
 		return (0);
 	while (nb > i && nb <= 2147395600)
