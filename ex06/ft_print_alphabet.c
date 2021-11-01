0d32af93216a7a3652551d491459da75a9dd82e4
mrantil <mrantil@student.hive.fi>
2021-11-01 16:36:12 +0200

    ft_print_alphabet

--- ex06/ft_print_alphabet.c
+++ ex06/ft_print_alphabet.c
@@ -6,16 +6,11 @@
 /*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 12:19:52 by mrantil           #+#    #+#             */
-/*   Updated: 2021/10/28 12:34:17 by mrantil          ###   ########.fr       */
+/*   Updated: 2021/11/01 16:35:51 by mrantil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
+void	ft_putchar(char c);
 
 void	ft_print_alphabet(void)
 {
