6dcb1dc6f484d237d9d4104794fe09fa44b69ef7
mrantil <mrantil@student.hive.fi>
2021-11-01 10:36:27 +0200

    ft_range

--- ex21/ft_range.c
+++ ex21/ft_range.c
@@ -1,40 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/01 10:31:52 by mrantil           #+#    #+#             */
+/*   Updated: 2021/11/01 10:35:56 by mrantil          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
-#include <stdio.h>
 
 int	*ft_range(int min, int max)
 {
-	int i;
-	int *arr;
-	int len;
+	int	i;
+	int	*arr;
+	int	len;
 
 	i = 0;
 	len = max - min;
 	if (min >= max)
-		return 0;
+		return (0);
 	arr = (int *)malloc(sizeof (int) * len);
 	if (arr)
+	{
 		while (min < max)
 		{
 			arr[i] = min;
 			min++;
 			i++;
 		}
-	return arr;
-}
-
-int	main(void)
-{
-	int i = 0;
-	int a = 25;
-	int b = 33;
-	int *range = ft_range(a, b);
-
-	while (a < b)
-	{
-		printf("range is: %d\n", range[i++]);
-		a++;
 	}
-	free(range);
-	return 0;
+	return (arr);
 }
-
